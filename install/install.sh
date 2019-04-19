61c5342884e64b5988850df05a89310aeee3d6cd
Amedeo Fadini <amefad@ve.ismar.cnr.it>
2019-04-19 23:52:57 +0200

    Updated install script with VIRTUAL_ENV variable

--- install/install.sh
+++ install/install.sh
@@ -6,13 +6,11 @@ set -x
 
 INITIAL_PATH='/opt/'
 
-#set VENV_PATH with virtualenv path (our is in opt/VirtEnv/iws
-VENV_PATH='/opt/VirtEnv/iws/'
 
 cd "$INITIAL_PATH"
 #pip install -e iws/
 cd "$INITIAL_PATH"iws/install
-patch "$VENV_PATH"local/lib/python2.7/site-packages/django/contrib/gis/geos/libgeos.py libgeos_patch.py
+patch "$VIRTUAL_ENV"/local/lib/python2.7/site-packages/django/contrib/gis/geos/libgeos.py libgeos_patch.py
 
 cd ..
 
@@ -28,4 +26,4 @@ pip install djangorestframework==3.5.4
 
 #make available python wheels installed for all users of group
 sudo chown -R $USER:www-data "$VENV_PATH"
-find "$VENV_PATH"lib/python2.7/site-packages/ -type d -exec chmod a+rwx {} \;
+find "$VIRTUAL_ENV"/lib/python2.7/site-packages/ -type d -exec chmod a+rwx {} \;
