41cae64ae6d2e0d49ef377cbd4ce8e298359bc72
Raoul <raouldonaldn@yahoo.it>
2019-07-04 20:54:52 +0200

    Added necessary files in Favourites

---
+++ istorm/app/containers/Favourites/reducer.js
@@ -0,0 +1,41 @@
+/*
+ *
+ * MapPage reducer
+ *
+ */
+import produce from 'immer';
+import { TOGGLE_DRAWER, OPEN_DRAWER, CLOSE_DRAWER, TOGGLE_DRAWER_MINI, CLOSE_DRAWER_MINI, OPEN_DRAWER_MINI } from './constants';
+
+export const initialState = {
+  drawer: {
+    open: true,
+    minimal: false,
+  }
+};
+
+/* eslint-disable default-case, no-param-reassign */
+const mapPageReducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case TOGGLE_DRAWER:
+        draft.drawer.open = !draft.drawer.open;
+        break;
+      case OPEN_DRAWER:
+        draft.drawer.open = true;
+        break;
+      case CLOSE_DRAWER:
+        draft.drawer.open = false;
+        break;
+      case TOGGLE_DRAWER_MINI:
+        draft.drawer.minimal = !draft.drawer.minimal;
+        break;
+      case OPEN_DRAWER_MINI:
+        draft.drawer.minimal = true;
+        break;
+      case CLOSE_DRAWER_MINI:
+        draft.drawer.minimal = false;
+        break;
+    };
+  });
+
+export default mapPageReducer;
