bdf713bdf878b2ebb26e32033479a543ad63e104
Giorgio INKODE <giorgio@inkode.it>
2019-07-05 19:24:55 +0200

    wip on favorites geojson

--- openistorm/favorites/urls.py
+++ openistorm/favorites/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-from oauth2_provider import views as oauthViews
-from views import FavoriteList
-from django.views.decorators.csrf import csrf_exempt
+from views import FavoriteList, FavoriteListGeoJson
 
 
 app_name = 'favorites'
 
 urlpatterns = [
-    url(r'', FavoriteList.as_view(), name="list"),
+    url(r'^geojson$', FavoriteListGeoJson.as_view(), name="geojson"),
+    url(r'^$', FavoriteList.as_view(), name="list"),
 ]
--- openistorm/favorites/views.py
+++ openistorm/favorites/views.py
@@ -1,13 +1,13 @@
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.generics import ListCreateAPIView, ListAPIView
 from rest_framework import status
 
 from .models import Favorite
 from .serializers import FavoriteSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.core.serializers import serialize
-from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 class FavoriteList(ListCreateAPIView):
     serializer_class =  FavoriteSerializer
@@ -19,7 +19,6 @@ class FavoriteList(ListCreateAPIView):
         user = self.request.user
         return qs.filter(user=user)
 
-    @csrf_exempt
     def create(self, request, *args, **kwargs):
         data = request.data
         data['user'] = self.request.user.pk
@@ -29,6 +28,24 @@ class FavoriteList(ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class FavoriteListGeoJson(ListAPIView):
+    serializer_class =  FavoriteSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Favorite.objects.all()
+
+    def get_queryset(self):
+        qs = super(FavoriteListGeoJson, self).get_queryset()
+        user = self.request.user
+        return qs.filter(user=user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serialized = serialize('geojson', queryset,
+                  geometry_field='position',
+                  fields=('id','title','address', 'position'))
+        return Response(json.loads(serialized))
 
 
 
