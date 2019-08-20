2d5df2c97bb03054b81f70af573a5600c5c3b02c
Amedeo Fadini <afadini@istorms.4c5bfbp4mtaedklxfx2poh33hh.ax.internal.cloudapp.net>
2019-08-20 21:20:15 +0000

    modified settings.py volumes nginx certs in docker-compose.yml

--- iws/settings.py
+++ iws/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS += (PROJECT_NAME,
                    PROJECT_NAME + '.tmes',
                    # 'rest_framework',
                    'rest_framework_swagger',
-                   'django_extensions',
+                   #'django_extensions',
 )
 
 # Location of url mappings
