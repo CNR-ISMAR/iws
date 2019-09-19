06b88f500e5f03ab30ef5de231709576ac76121b
Giorgio INKODE <giorgio@inkode.it>
2019-09-19 20:15:07 +0200

    import 2015, Feb

--- openistorm/layers/utils.py
+++ openistorm/layers/utils.py
@@ -119,8 +119,8 @@ class NCToImg:
                     m = band1.GetMetadata()
 
                     # ts = datetime.utcfromtimestamp(int(m['NETCDF_DIM_time']) + since).strftime('%Y%m%d-%H%M00')
-                    ts = datetime.utcfromtimestamp( (int(m['NETCDF_DIM_time'])*3600, pytz.utc) + since).strftime('%s')
-                    json_time = datetime.utcfromtimestamp( (int(m['NETCDF_DIM_time'])*3600, pytz.utc) + since).strftime('%Y-%m-%dT%H:%M.000Z')
+                    ts = datetime.utcfromtimestamp( (int(m['NETCDF_DIM_time'])*3600) + since, pytz.utc).strftime('%s')
+                    json_time = datetime.utcfromtimestamp( (int(m['NETCDF_DIM_time'])*3600) + since, pytz.utc).strftime('%Y-%m-%dT%H:%M.000Z')
                     print("\n\n")
                     print(since)
                     print("\n\n"+m['NETCDF_DIM_time']+"\n\n")
