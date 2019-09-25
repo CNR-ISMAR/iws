e546f2d5cd155027cbc3302f719741bae4efe238
Giorgio INKODE <giorgio@inkode.it>
2019-09-25 15:25:26 +0200

    chart

--- istorm/app/utils/labels.js
+++ istorm/app/utils/labels.js
@@ -1,6 +1,21 @@
 export default {
-  sea_level: "Sea Level",
-  wsh: "Wave Height",
-  wmp: "Wave Period",
-  wmd: "Wave Direction"
+  "sea_level": "Sea Level",
+  "wsh": "Wave Height",
+  "wmp": "Wave Period",
+  "wmd": "Wave Direction",
+
+  lines: {
+  "sea_level-min": "Sea Level min",
+  "wsh-min": "Wave Height min",
+  "wmp-min": "Wave Period min",
+  "wmd-min": "Wave Direction min",
+  "sea_level-max": "Sea Level max",
+  "wsh-max": "Wave Height max",
+  "wmp-max": "Wave Period max",
+  "wmd-max": "Wave Direction max",
+  "sea_level-mean": "Sea Level mean",
+  "wsh-mean": "Wave Height mean",
+  "wmp-mean": "Wave Period mean",
+  "wmd-mean": "Wave Direction mean",
+  }
 }
