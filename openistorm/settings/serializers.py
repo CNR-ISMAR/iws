1bbc5764ce046cc022820a5b41fb0f4aa137484b
Giorgio INKODE <giorgio@inkode.it>
2019-10-18 13:11:26 +0200

    fix user settings view

--- openistorm/settings/serializers.py
+++ openistorm/settings/serializers.py
@@ -4,7 +4,8 @@ from models import Setting
 class SettingSerializer(serializers.ModelSerializer):
     sl_reference = serializers.IntegerField
     sl_notification_threshold = serializers.IntegerField
+    user_id = serializers.UUIDField
     class Meta:
         model = Setting
-        fields = ('sl_reference', 'sl_notification_threshold')
+        fields = ('sl_reference', 'sl_notification_threshold', 'user_id')
 
