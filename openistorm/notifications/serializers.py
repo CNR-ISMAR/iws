83b983575d55b3ffd46d5fde18f53e232af3bbec
Giorgio INKODE <giorgio@inkode.it>
2019-10-21 19:23:43 +0200

    TEST CON NOTIFICHE SEMPRE AVANTI

--- openistorm/notifications/serializers.py
+++ openistorm/notifications/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from models import Notification
 from django.contrib.gis.geos import Point
+import datetime
 
 class NotificationSerializer(serializers.ModelSerializer):
     longitude = serializers.SerializerMethodField()
@@ -9,7 +10,9 @@ class NotificationSerializer(serializers.ModelSerializer):
     position = Point
 
     def get_time(self, instance):
-        return instance.time.isoformat()[0:19]+'.000Z'
+        return (datetime.datetime.now().replace(minute=0, second=0, microsecond=0) + datetime.timedelta(hours=2)).isoformat()[0:19]+'.000Z'
+        # return instance.time.isoformat()[0:19]+'.000Z' if instance.time else None
+        return instance.time.isoformat()[0:19]+'.000Z' if instance.time else None
 
     def get_latitude(self, instance):
         return instance.position.y
