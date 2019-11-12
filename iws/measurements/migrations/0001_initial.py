114a032601af71df9f11c0b18029c1c282ba0c81
Giorgio INKODE <giorgio@inkode.it>
2019-11-12 19:23:59 +0100

    removing dss_pharos

--- iws/measurements/migrations/0001_initial.py
+++ iws/measurements/migrations/0001_initial.py
@@ -2,8 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
-import django.contrib.postgres.fields
 import django.contrib.gis.db.models.fields
 
 
@@ -18,23 +16,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('label', models.CharField(max_length=150)),
-                ('geo', django.contrib.gis.db.models.fields.PointField(srid=4326, null=True)),
+                ('geo', django.contrib.gis.db.models.fields.PointField(srid=4326)),
             ],
         ),
         migrations.CreateModel(
             name='Measure',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('timestamp', models.DateTimeField(db_index=True)),
                 ('value', models.FloatField()),
                 ('location', models.ForeignKey(to='measurements.Location')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Network',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('label', models.CharField(max_length=150)),
             ],
         ),
         migrations.CreateModel(
@@ -52,27 +42,14 @@ class Migration(migrations.Migration):
                 ('label', models.CharField(max_length=150)),
             ],
         ),
-
-        migrations.CreateModel(
-            name='Serie',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('stats_mean', models.FloatField(null=True)),
-                ('stats_outliers', django.contrib.postgres.fields.ArrayField(null=True, base_field=models.IntegerField(), size=None)),
-                ('location', models.ForeignKey(to='measurements.Location')),
-                ('network', models.ForeignKey(to='measurements.Network')),
-                ('parameter', models.ForeignKey(to='measurements.Parameter')),
-                ('sensor', models.ForeignKey(to='measurements.Sensor')),
-            ],
-        ),
         migrations.AddField(
             model_name='measure',
-            name='serie',
-            field=models.ForeignKey(to='measurements.Serie'),
+            name='parameter',
+            field=models.ForeignKey(to='measurements.Parameter'),
+        ),
         migrations.AddField(
             model_name='measure',
             name='sensor',
             field=models.ForeignKey(to='measurements.Sensor'),
-
         ),
     ]
