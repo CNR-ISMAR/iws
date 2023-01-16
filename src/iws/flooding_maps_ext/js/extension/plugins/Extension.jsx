80f8aa3637cddbbfe4e17b230b8a8c1fcebb4b4d
Niccolò Cantù <niccolocantu@gmail.com>
2023-01-16 10:28:55 +0100

    add mapstore extension

---
+++ src/iws/flooding_maps_ext/js/extension/plugins/Extension.jsx
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2021, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card, Glyphicon } from 'react-bootstrap';
+import { createSelector } from 'reselect';
+import Rx from "rxjs";
+
+import { connect } from 'react-redux';
+
+/* EPICS */
+
+/* ACTIONS */
+// mapstore
+import { zoomToExtent } from "@mapstore/framework/actions/map";
+import { setControlProperty, toggleControl } from "@mapstore/framework/actions/controls";
+
+/* COMPONENTS */
+// mapstore
+import DockablePanel from '@mapstore/framework/components/misc/panels/DockablePanel';
+import Message from "@mapstore/framework/components/I18N/Message";
+
+/* SELECTORS */
+// mapstore
+import { mapSelector } from '@mapstore/framework/selectors/map';
+import { layersSelector, groupsSelector, rawGroupsSelector } from '@mapstore/framework/selectors/layers';
+// custom
+import { geotourEnabledSelector } from '../selectors/geotour';
+
+/* CUSTOM LOCAL STYLE */
+import '@js/extension/assets/style.css';
+
+/**
+* Extension is a functional component, i.e. it's a function that returns a React component
+* @prop {object} props passed to the component, in this case from the connect function
+* @returns {React.Component} the component to be used inside the plugin
+*/
+function Extension({           
+    id = "geo-tour-plugin",
+    dockStyle = {},
+    // coming from connect function connected to the store
+    enabled = false,
+    showCloseButton = true,
+    // use tmap from connect if we need (eg for get center)
+    groups,
+    //  coming from connect function mapDispatch
+    onClose = () => {},
+    flyTo = () => {}
+}) {
+
+    // Add code to do something to change what we have in what we Render with return 
+   
+   const [flyToEnabled, setFlyToEnabled] = useState(true);
+   return enabled ? (<DockablePanel
+    open={enabled}
+    // dock // If dock -> the panel is placed in the following position
+    // position="right"
+    bsStyle="primary"
+    draggable={false}
+    size={200}
+    title={<Message msgId="Places"/>}
+    onClose={showCloseButton ? onClose : null}
+    // glyph="globe"
+    style={dockStyle}
+ >
+        { groups.map((group) => {
+        //   console.log(group.title)
+            return (
+                <div className="gd-center">
+                <Button variant="primary" style={{ width: "80%" }} disabled={!flyToEnabled} onClick={() => {
+                    // console.log(group.nodes[0].bbox.bounds)    
+                    // console.log(group.nodes[0].bbox.crs)
+                    flyTo(group.nodes[0].bbox.bounds, group.nodes[0].bbox.crs, 20, {duration: 1000});
+                    }}> {group.title} </Button>
+                </div>    
+            );            
+        }) }
+     </DockablePanel>) : null;
+}
+
+/** Define the types of props */
+Extension.propTypes = {};
+ 
+/** Set default props */
+Extension.defaultProps = {}
+
+const ConnectedExtension = connect(   
+    createSelector([
+        /** SELECTORS */
+        geotourEnabledSelector,
+        groupsSelector,
+       ], (enabled, groups) => ({
+           enabled,
+           groups
+    })),
+   {
+      /** ACTIONS */
+      // Custom
+      // mapstore
+        flyTo: zoomToExtent,
+        onClose: setControlProperty.bind(null, "geotour", "enabled", false, false)
+   })(Extension);
+
+
+/** Plugin export */   
+export default {
+    name: __MAPSTORE_EXTENSION_CONFIG__.name,
+    component: ConnectedExtension,    
+    reducers: {},
+    epics: {},
+    containers: {
+      Toolbar: {
+        name: "SampleExtension",
+        position: 10,
+        text: "GO",
+        doNotHide: true,
+        action: toggleControl.bind(null, 'geotour', null),
+        priority: 1
+     }
+    }
+};
---
+++ src/iws/flooding_maps_ext/js/extension/selectors/geotour.js
@@ -0,0 +1,30 @@
+import { head, isArray } from 'lodash';
+
+export const sampleExtensionSelector  = state => state?.sampleExtension?.value;
+export const geotourEnabledSelector  = state => state?.controls?.geotour?.enabled;
+
+/**
+ * this selector allows you to fetch all the layers from the dedicated part of the store i.e. "layers",
+ * note the initial destructuring
+ * export const layersSelector = (state) =>
+ * vs
+ * export const layersSelector = ({layers, config} = {}) =>
+*/
+export const layersSelector = ({layers, config} = {}) => layers && isArray(layers) ? layers : layers && layers.flat || config && config.layers || [];
+
+/**
+ * this selector leverage the previous selector in order to obtain a layer given an intial id
+*/
+export const getLayerFromId = (state, id) => head(layersSelector(state).filter(l => l.id === id));
+
+/**
+ * USAGE:
+ *
+ * in epics
+ * const layer = getLayerFromId(store.getState(), myId);
+
+ * in connect
+ * state => getLayerFromId(state, myId);
+ * or
+ * state => getLayerFromId(state, getSpecificId(state));
+*/
