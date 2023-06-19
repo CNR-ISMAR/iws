5767ffc44d7ec8d3199c41de0142ce2b82c88e5c
Amedeo Fadini <amedeo.fadini@ve.ismar.cnr.it>
2023-06-20 00:13:22 +0200

    Removed local measurements and update settings.py for package measurements

--- src/iws/measurements/sandbox.py
+++
@@ -1,8 +0,0 @@
-from iws.measurements.models import *
-
-
-p = Parameter.objects.get(label='SLEV')
-series = Serie.objects.filter(parameter=p)
-for s in series:
-    # .filter(location__label='Itea'):
-    s.set_mean(threshold=1)
